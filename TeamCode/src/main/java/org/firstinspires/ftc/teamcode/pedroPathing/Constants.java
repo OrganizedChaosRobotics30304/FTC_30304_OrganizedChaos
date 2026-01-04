5843b2aa487c3fa4c8c657c99a9d13287687afb2
RISE-r0b0t1cs <roboticinnovationinc@gmail.com>
2026-01-04 15:45:01 -0500

    Update Constants.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/Constants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/Constants.java
@@ -12,7 +12,9 @@ import com.qualcomm.robotcore.hardware.HardwareMap;
 
 public class Constants {
     public static FollowerConstants followerConstants = new FollowerConstants()
-            .mass(10.07);
+            .mass(10.07)
+            .forwardZeroPowerAcceleration(271.529341010438)
+            .lateralZeroPowerAcceleration(-691.9042000980687);
 
     public static MecanumConstants driveConstants = new MecanumConstants()
             .maxPower(1)
@@ -20,10 +22,12 @@ public class Constants {
             .rightRearMotorName("rightRearDriveMotor")
             .leftRearMotorName("leftRearDriveMotor")
             .leftFrontMotorName("leftFrontDriveMotor")
-            .leftFrontMotorDirection(DcMotorSimple.Direction.REVERSE)
-            .leftRearMotorDirection(DcMotorSimple.Direction.REVERSE)
-            .rightFrontMotorDirection(DcMotorSimple.Direction.FORWARD)
-            .rightRearMotorDirection(DcMotorSimple.Direction.FORWARD);
+            .leftFrontMotorDirection(DcMotorSimple.Direction.FORWARD)
+            .leftRearMotorDirection(DcMotorSimple.Direction.FORWARD)
+            .rightFrontMotorDirection(DcMotorSimple.Direction.REVERSE)
+            .rightRearMotorDirection(DcMotorSimple.Direction.REVERSE)
+           .xVelocity(156.423232687786)
+            .yVelocity(127.133538448267);
 
     public static DriveEncoderConstants localizerConstants = new DriveEncoderConstants()
             .rightFrontMotorName("rightFrontDriveMotor")
@@ -32,11 +36,13 @@ public class Constants {
             .leftFrontMotorName("leftFrontDriveMotor")
             .leftFrontEncoderDirection(Encoder.FORWARD)
             .leftRearEncoderDirection(Encoder.FORWARD)
-            .rightFrontEncoderDirection(Encoder.FORWARD)
-            .rightRearEncoderDirection(Encoder.FORWARD)
+            .rightFrontEncoderDirection(Encoder.REVERSE)
+            .rightRearEncoderDirection(Encoder.REVERSE)
             .robotLength(13.94)
             .robotWidth(15.38)
-            ;
+            .forwardTicksToInches(0.0185477677767398)
+            .strafeTicksToInches(0.0152967193003097)
+            .turnTicksToInches(0.0122837607070128);
 
 
     public static PathConstraints pathConstraints = new PathConstraints(0.99, 100, 1, 1);
